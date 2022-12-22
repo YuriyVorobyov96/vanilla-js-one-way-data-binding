1a569a1e264693d1d8cc584ea908ef65b1e9bad1
YuriyVorobyov96 <darthrevan1333@gmail.com>
2022-12-22 22:24:10 +0300

    one-way binding

---
+++ index.js
@@ -0,0 +1,22 @@
+createdBindedVariable("counter");
+counter = 0;
+
+function updateCounter(val) {
+  counter += val;
+}
+
+function createdBindedVariable(targetId) {
+  let newVal = null;
+
+  Object.defineProperty(window, targetId, {
+    set: value => {
+      newVal = value;
+
+      let targetRef = document.getElementById(targetId);
+      targetRef.innerHTML = value;
+    },
+    get: () => {
+      return newVal;
+    }
+  })
+}
